f9aee1afd48148bff182afaa8362f34cc423da6d
Your Name <you@example.com>
2024-09-12 06:30:01 -0400

    Get all books implementation

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,7 +12,7 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  return res.status(300).json(JSON.stringify(books, null, 4));
+  return res.status(300).json(books);
 });
 
 // Get book details based on ISBN

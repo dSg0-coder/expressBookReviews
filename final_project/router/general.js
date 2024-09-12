b6ee33497ce61a321acdaf788c021fb65631e555
Your Name <you@example.com>
2024-09-12 08:38:31 -0400

    Get books by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -29,8 +29,9 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const filteredBooks = Object.entries(books).filter(
+      ([key, value]) => { return value.title === req.params.title })
+    return res.status(300).json(filteredBooks);
 });
 
 //  Get book review

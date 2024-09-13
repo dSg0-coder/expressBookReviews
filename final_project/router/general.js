8254c107cac0bf509f9a6b9dc00d222aaa4cd808
Your Name <you@example.com>
2024-09-13 04:43:22 -0400

    Asyn/await in genral.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,24 +25,25 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/', async function (req, res) {
   return res.status(300).json(books);
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  return res.status(300).json(books[req.params.isbn]);
+public_users.get('/isbn/:isbn', async function (req, res) {
+  const filteredBooks = await books[req.params.isbn];
+  return res.status(300).json(filteredBooks);
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
   const filteredBooks = Object.entries(books).filter(
     ([key, value]) => { return value.author === req.params.author })
   return res.status(300).json(filteredBooks);
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
     const filteredBooks = Object.entries(books).filter(
       ([key, value]) => { return value.title === req.params.title })
     return res.status(300).json(filteredBooks);
